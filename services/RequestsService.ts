d7ff869a2174bea58e23ffc42ba795ff83b93041
Carsten Gräf <evandor@gmail.com>
2025-03-10 12:27:16 +0100

    wip

--- services/RequestsService.ts
+++ services/RequestsService.ts
@@ -1,7 +1,5 @@
-import _ from 'lodash'
 import { RequestInfo } from 'src/requests/models/RequestInfo'
 import RequestsPersistence from 'src/requests/persistence/RequestsPersistence'
-import { TabAndTabsetId } from 'src/tabsets/models/TabAndTabsetId'
 import { useTabsetsStore } from 'src/tabsets/stores/tabsetsStore'
 
 let db: RequestsPersistence = null as unknown as RequestsPersistence
@@ -14,13 +12,40 @@ export function useRequestsService() {
     // console.debug(` ...initialized requests: Service`, '✅')
   }
 
-  const logWebRequest = (details: chrome.webRequest.WebResponseHeadersDetails) => {
+  const logWebRequest = async (details: chrome.webRequest.WebResponseHeadersDetails) => {
     const tabsForUrl = useTabsetsStore().tabsForUrl(details.url)
-    _.forEach(tabsForUrl, (tabAndTabsetId: TabAndTabsetId) => {
-      db.saveRequest(new RequestInfo(tabAndTabsetId.tab.id, details.statusCode, details.responseHeaders || []))
-        //.then(() => console.debug('added request'))
-        .catch((err) => console.warn('error in logWebRequest', err))
-    })
+
+    function iFrameDenied(responseHeaders: chrome.webRequest.HttpHeader[] | undefined) {
+      const securityPolicyHeader = details.responseHeaders?.find(
+        (h: chrome.webRequest.HttpHeader) => h.name.toLowerCase() === 'content-security-policy',
+      )
+      if (securityPolicyHeader && "frame-ancestors 'self'" === securityPolicyHeader.value) {
+        return true
+      }
+      const xFrameOptions = details.responseHeaders?.find(
+        (h: chrome.webRequest.HttpHeader) => h.name.toLowerCase() === 'x-frame-options',
+      )
+      if (xFrameOptions && 'DENY' === xFrameOptions.value) {
+        return true
+      }
+      return false
+    }
+
+    //_.forEach(tabsForUrl, (tabAndTabsetId: TabAndTabsetId) => {
+    for (const tabAndTabsetId of tabsForUrl) {
+      try {
+        await db.saveRequest(new RequestInfo(tabAndTabsetId.tab.id, details.statusCode, details.responseHeaders || []))
+        console.debug('adding iframe info to tab')
+
+        if (iFrameDenied(details.responseHeaders)) {
+          tabAndTabsetId.tab.useInIframe = false
+          const ts = useTabsetsStore().getTabset(tabAndTabsetId.tabsetId)
+          await useTabsetsStore().saveTabset(ts!)
+        }
+      } catch (err: any) {
+        console.warn('error in logWebRequest', err)
+      }
+    }
   }
 
   const getWebRequestFor = (tabId: string): Promise<RequestInfo> => {
